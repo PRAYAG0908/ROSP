42c6d9544c5c3102734d4a4e081793b203c3aef1
Rushi1907 <rushikeshd1907@gmail.com>
2024-10-27 16:28:01 +0530

    Index.js and App.js Commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import Navigation from './customer/component/Navigation/Navigation';
+import HomePage from './customer/pages/HomePage/HomePage';
+import Footer from './customer/component/Footer/Footer';
+import Product from './customer/component/Product/Product';
+import ProductDetails from './customer/component/ProductDetails/ProductDetails';
+import Cart from './customer/component/Cart/Cart';
+import Checkout from './customer/component/Checkout/Checkout';
+import Order from './customer/component/Order/Order';
+import OrderDetails from './customer/component/Order/OrderDetails';
+import { Route, Routes } from 'react-router-dom';
+import CustomerRoutes from './customer/Routes/CustomerRoutes';
+
+function App() {
+  return (
+    <div className="">
+
+      <Routes>
+      <Route path='/*' element={<CustomerRoutes/>}></Route>
+
+
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
